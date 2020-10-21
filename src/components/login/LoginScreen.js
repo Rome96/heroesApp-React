26173d691a7420f819275957bec3396c92d605af
Turiano Romero <romero.guzman18@gmail.com>
2020-10-20 22:44:34 -0500

    history push/replace react-router

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const LoginScreen = () => {
+const LoginScreen = ({ history }) => {
+
+  const handleLogin = () => {
+    // history.push('/')
+    history.replace('/')
+  }
+
   return (
-    <div>
-      <h3>Login Screen</h3>
+    <div className='container mt-5'>
+      <h3>Login</h3> <hr/>
+
+      <button
+        onClick={handleLogin}
+        className='btn btn-primary'
+      >
+        Login
+      </button>
     </div>
   );
 };
