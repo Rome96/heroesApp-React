e262773743343f3817b98b207fbf7e3f0ed9bd34
Turiano Romero <romero.guzman18@gmail.com>
2020-10-21 23:49:06 -0500

    useMemo

--- src/components/heroes/HeroesList.js
+++ src/components/heroes/HeroesList.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { getHeroesByPublisher } from '../selectors/getHeroesByPublisher';
 import HeroCard from './HeroCard';
 
 const HeroesList = ({ publisher  }) => {
-  const heroes = getHeroesByPublisher(publisher)
-
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
   return (
     <div className="card-columns">
       {
