78d8574d4752f5d62df34366874428a5adba5e9b
Turiano Romero <romero.guzman18@gmail.com>
2020-10-21 23:39:16 -0500

    styles in component heroesScreen

--- src/components/heroes/HeroesScreen.js
+++ src/components/heroes/HeroesScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams, Redirect } from 'react-router-dom';
 import { getHeroById } from '../selectors/getHeroById';
 
-const HeroesScreen = () => {
+const HeroesScreen = ({history}) => {
   const { heroeId } = useParams();
   const hero = getHeroById(heroeId)
 
@@ -10,12 +10,45 @@ const HeroesScreen = () => {
     return <Redirect to='/' />
   }
 
-  const { superhero, alter_ego, first_appearance, characters } = hero
+  const handleReturn = () => {
+    if (history.length <= 2) {
+      history.push('/')
+    } else {
+      history.goBack();
+    }
+  }
+
+  const { superhero, alter_ego, first_appearance, characters, publisher } = hero
 
   console.log(superhero)
   return (
-    <div>
-      <h3>Heroes Screen</h3>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          alt={superhero}
+          className="img-thumbnail"
+          src={`../assets/heroes/${heroeId}.jpg`}
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b> {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b> {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance:</b> {first_appearance}
+          </li>
+        </ul> <hr/>
+        <h5> Characters </h5>
+        <p>{characters}</p>
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Return
+        </button>
+      </div>
     </div>
   );
 };
