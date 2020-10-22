edf74567158cd0fdf7497b8726eca65d2852b03e
Turiano Romero <romero.guzman18@gmail.com>
2020-10-22 00:03:13 -0500

    animation component HeroesScreen

--- src/components/heroes/HeroesList.js
+++ src/components/heroes/HeroesList.js
@@ -5,7 +5,7 @@ import HeroCard from './HeroCard';
 const HeroesList = ({ publisher  }) => {
   const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
   return (
-    <div className="card-columns">
+    <div className="card-columns animate__animated animate__fadeIn">
       {
         heroes.map(data => (
           <HeroCard
--- src/components/heroes/HeroesScreen.js
+++ src/components/heroes/HeroesScreen.js
@@ -20,37 +20,34 @@ const HeroesScreen = ({history}) => {
 
   const { superhero, alter_ego, first_appearance, characters, publisher } = hero
 
-  console.log(superhero)
-  return (
-    <div className="row mt-5">
-      <div className="col-4">
-        <img
-          alt={superhero}
-          className="img-thumbnail"
-          src={`../assets/heroes/${heroeId}.jpg`}
-        />
-      </div>
-      <div className="col-8">
-        <h3>{superhero}</h3>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <b>Alter ego:</b> {alter_ego}
-          </li>
-          <li className="list-group-item">
-            <b>Publisher:</b> {publisher}
-          </li>
-          <li className="list-group-item">
-            <b>First appearance:</b> {first_appearance}
-          </li>
-        </ul> <hr/>
-        <h5> Characters </h5>
-        <p>{characters}</p>
-        <button className="btn btn-outline-info" onClick={handleReturn}>
-          Return
-        </button>
-      </div>
+  return <div className="row mt-5">
+    <div className="col-4">
+      <img
+        alt={superhero}
+        className="img-thumbnail  animate__animated animate__backInLeft"
+        src={`../assets/heroes/${heroeId}.jpg`}
+      />
     </div>
-  );
+    <div className="col-8">
+      <h3>{superhero}</h3>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">
+          <b>Alter ego:</b> {alter_ego}
+        </li>
+        <li className="list-group-item">
+          <b>Publisher:</b> {publisher}
+        </li>
+        <li className="list-group-item">
+          <b>First appearance:</b> {first_appearance}
+        </li>
+      </ul> <hr/>
+      <h5> Characters </h5>
+      <p>{characters}</p>
+      <button className="btn btn-outline-info" onClick={handleReturn}>
+        Return
+      </button>
+    </div>
+  </div>
 };
 
 export default HeroesScreen;
