88a3796932eedc8801068879d451c9c4782b1b2f
Turiano Romero <romero.guzman18@gmail.com>
2020-10-23 00:33:37 -0500

    aplicar filtro con base a queryString

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import queryString from 'query-string';
 import HeroCard from '../heroes/HeroCard';
 import { useForm } from '../hooks/useForm';
 import { useLocation } from 'react-router-dom';
+import { getHeroesByName } from '../selectors/getHeroesByName';
+import { heroes } from '../data/heroes';
 
 const SearchScreen = ({history}) => {
   const location = useLocation();
@@ -15,41 +17,43 @@ const SearchScreen = ({history}) => {
     history.push(`?q=${searchText}`)
   };
 
-  const turi = []
+  const heroe = useMemo(() => getHeroesByName(q), [q])
 
-  return (
-    <div>
-      <h2>Seach..</h2> <hr />
-      <div className="row">
-        <div className="col-5">
-          <h4>Search Form</h4> <hr />
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="searchText"
-              value={searchText}
-              autoComplete="off"
-              className="form-control"
-              placeholder="Find your hero"
-              onChange={handleInputChange}
-            />
-            <button
-              type="submit"
-              className="btn m-1 btn-block btn-outline-primary"
-            >
-              Search...
-            </button>
-          </form>
-        </div>
-        <div className="col-7">
-          <h4>Results</h4> <hr />
-          {turi.map((hero) => (
-            <HeroCard key={hero.id} {...hero} />
-          ))}
-        </div>
-      </div>
+  return <div className="row mt-5">
+    <div className="col-5">
+      <h4>Search Form</h4> <hr />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchText"
+          value={searchText}
+          autoComplete="off"
+          className="form-control"
+          placeholder="Find your hero"
+          onChange={handleInputChange}
+        />
+        <button
+          type="submit"
+          className="btn m-1 btn-block btn-outline-primary"
+        >
+          Search...
+        </button>
+      </form>
     </div>
-  );
+    <div className="col-7">
+      <h4>Results</h4> <hr />
+
+      { (q === '') && <div className="alert alert-info">Search a hero</div> }
+
+      {
+        (q !== '' && heroe.length === 0) && <div className="alert alert-danger">
+          There is no a hero with {q}
+        </div> 
+      }
+
+      { heroe.map((hero) =>  <HeroCard key={hero.id} {...hero} /> ) }
+    </div>
+  </div>
 }
 
 export default SearchScreen
--- src/components/selectors/getHeroesBySuperhero.js
+++
@@ -1,5 +0,0 @@
-import { heroes } from "../data/heroes";
-
-export const getHeroesBySuperhero = (superhero) => {
-  return heroes.filter((hero) => hero.superhero === superhero);
-};
