81f10dea9d6924c765be16f1028b512df135597c
Turiano Romero <romero.guzman18@gmail.com>
2020-10-23 22:31:58 -0500

    Context and Reducer in application

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { AppRouter } from './routers/AppRouter'
+import { AuthContext } from './components/auth/AuthContext'
+import { authReducer } from './components/auth/authReducer'
+
+const init = () => {
+	return JSON.parse(localStorage.getItem('user')) || { logged: false };
+};
 
 const HeroesApp = () => {
-	return <AppRouter />
-}
 
-export default HeroesApp
+	const [user, dispatch] = useReducer(authReducer, {}, init);
+
+	return <AuthContext.Provider value={{user, dispatch}}>
+		<AppRouter />
+	</AuthContext.Provider>
+};
+
+export default HeroesApp;
